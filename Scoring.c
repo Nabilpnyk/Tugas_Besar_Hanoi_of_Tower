adde5ec8ac91a040e158bf9028049a73a14e3128
Orderxxx <ivansetiawan2975@gmail.com>
2024-12-06 21:49:57 +0700

    Update

---
+++ Scoring.c
@@ -0,0 +1,31 @@
+#include "Hanoi.h"
+
+float Scoring(int Langkah, int Kecepatan, int choice){
+	const float bobotLangkah = 0.5;
+	const float bobotKecepatan = 0.5;
+	float scoreLangkah;
+	float scoreKecepatan;
+	float totalScore;
+	
+	switch (choice){
+		case 0:
+			scoreLangkah = (Langkah/6);
+			scoreKecepatan = (45/Kecepatan);
+			break;
+		case 1:
+			scoreLangkah = (Langkah/4);
+			scoreKecepatan = (93/Kecepatan);
+			break;
+		case 2:
+			scoreLangkah = (Langkah/2);
+			scoreKecepatan = (189/Kecepatan);
+			break;
+		case 3:	
+			scoreLangkah = (Langkah/1);
+			scoreKecepatan = (381/Kecepatan);
+			break;
+	}
+	totalScore = (bobotLangkah * scoreLangkah) + (bobotKecepatan * scoreKecepatan);
+	
+	return totalScore;
+}
