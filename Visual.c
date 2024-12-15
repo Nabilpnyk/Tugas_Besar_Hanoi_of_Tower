f6cbf4b2a7db1ece64abe2e60b3a2d16407f54fb
Orderxxx <ivansetiawan2975@gmail.com>
2024-12-15 13:56:03 +0700

    Update and rename

--- Visual.c
+++ Visual.c
@@ -1,14 +1,42 @@
-#include "Hanoi.c"
+#include "Hanoi.h"
 
+int i,j,n;
+//================= INISIALISASI GLOBAL ========================
+SMALL_RECT Balok[3][7];
+
+COORD O = {0,0};
+
+CHAR_INFO F1[3], F2[5], F3[7], F4[9], F5[11], F6[13], F7[15], Ft[20],E1[3], E2[5], E3[7], E4[9], E5[11], E6[13], E7[15], FB[57];
+
+COORD Size[9];
+
+CHAR_INFO *CC[16]={F1, F2, F3, F4, F5, F6, F7, Ft, E1, E2, E3, E4, E5, E6, E7, FB};
+
+SMALL_RECT Tf1 = {39, 15, 39, 25}, Tf2 = {59, 15, 59, 25}, Tf3 = {79, 15, 79, 25}, Tb = {31, 24, 88, 24};
+
+HANDLE Out()
+{
+	return (GetStdHandle(STD_OUTPUT_HANDLE));
+}
+
+void gotoxy(int x, int y) 
+{
+  COORD Point;
+  Point.X = x;
+  Point.Y = y;
+  SetConsoleCursorPosition(Out(), Point);
+}
+
+//=============== VISUAL ===================
 void SizeRect()
 {
-	Size[0] = (COORD){3, 1};
-	Size[1] = (COORD){5, 1};
-	Size[2] = (COORD){7, 1};
-	Size[3] = (COORD){9, 1};
-	Size[4] = (COORD){11, 1};
-	Size[5] = (COORD){13, 1};
-	Size[6] = (COORD){15, 1};
+    Size[0] = (COORD){3, 1};
+    Size[1] = (COORD){5, 1};
+    Size[2] = (COORD){7, 1};
+    Size[3] = (COORD){9, 1};
+    Size[4] = (COORD){11, 1};
+    Size[5] = (COORD){13, 1};
+    Size[6] = (COORD){15, 1};
 	Size[7] = (COORD){1, 10};
 	Size[8] = (COORD){57, 1};
 }
@@ -22,7 +50,7 @@ void SetAwal(HANDLE Out, int n)
 	for (j=0; j<n; j++)
 	{
 		WriteConsoleOutput(Out,CC[j],Size[j],O, &Balok[0][j]);
-	}
+	}	
 
 }
 
@@ -46,52 +74,52 @@ void SetChar()
 {
 	for(i=0; i<10; i++)
 	{
-		Ft[i].Char.AsciiChar = 219;
+		Ft[i].Char.AsciiChar = 219; 
 		Ft[i].Attributes = FOREGROUND_BLUE|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_INTENSITY;
 	}
-
+	
 	for(i=0; i<3; i++)
 	{
-		F1[i].Char.AsciiChar = 219;
+		F1[i].Char.AsciiChar = 219; 
 		F1[i].Attributes = 7;
 	}
-
+	
 	for(i=0; i<5; i++)
 	{
-		F2[i].Char.AsciiChar = 219;
+		F2[i].Char.AsciiChar = 219; 
 		F2[i].Attributes = 8;
 	}
-
+	
 	for(i=0; i<7; i++)
 	{
-		F3[i].Char.AsciiChar = 219;
+		F3[i].Char.AsciiChar = 219; 
 		F3[i].Attributes = 9;
 	}
-
+	
 	for(i=0; i<9; i++)
 	{
-		F4[i].Char.AsciiChar = 219;
+		F4[i].Char.AsciiChar = 219; 
 		F4[i].Attributes = 10;
 	}
-
+	
 	for(i=0; i<11; i++)
 	{
-		F5[i].Char.AsciiChar = 219;
+		F5[i].Char.AsciiChar = 219; 
 		F5[i].Attributes = 11;
 	}
-
+	
 	for(i=0; i<13; i++)
 	{
-		F6[i].Char.AsciiChar = 219;
+		F6[i].Char.AsciiChar = 219; 
 		F6[i].Attributes = 12;
 	}
-
+	
 	for(i=0; i<15; i++)
 	{
-		F7[i].Char.AsciiChar = 219;
+		F7[i].Char.AsciiChar = 219; 
 		F7[i].Attributes = 13;
 	}
-
+	
  	for(i=0;i<3;i++)
     {
         E1[i].Char.AsciiChar=' ';
@@ -99,7 +127,7 @@ void SetChar()
     }
     E1[1].Char.AsciiChar= 219;
     E1[1].Attributes=FOREGROUND_BLUE|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_INTENSITY;
-
+    
     for(i=0;i<5;i++)
     {
         E2[i].Char.AsciiChar=' ';
@@ -107,7 +135,7 @@ void SetChar()
     }
     E2[2].Char.AsciiChar= 219;
     E2[2].Attributes=FOREGROUND_BLUE|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_INTENSITY;
-
+    
     for(i=0;i<7;i++)
     {
         E3[i].Char.AsciiChar=' ';
@@ -115,7 +143,7 @@ void SetChar()
     }
     E3[3].Char.AsciiChar= 219;
     E3[3].Attributes=FOREGROUND_BLUE|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_INTENSITY;
-
+    
 	for(i=0;i<9;i++)
     {
         E4[i].Char.AsciiChar=' ';
@@ -123,7 +151,7 @@ void SetChar()
     }
     E4[4].Char.AsciiChar= 219;
     E4[4].Attributes=FOREGROUND_BLUE|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_INTENSITY;
-
+    
     for(i=0;i<11;i++)
     {
         E5[i].Char.AsciiChar=' ';
@@ -131,7 +159,7 @@ void SetChar()
     }
     E5[5].Char.AsciiChar= 219;
     E5[5].Attributes=FOREGROUND_BLUE|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_INTENSITY;
-
+    
     for(i=0;i<13;i++)
     {
         E6[i].Char.AsciiChar=' ';
@@ -139,7 +167,7 @@ void SetChar()
     }
     E6[6].Char.AsciiChar= 219;
     E6[6].Attributes=FOREGROUND_BLUE|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_INTENSITY;
-
+    
     for(i=0;i<15;i++)
     {
     	E7[i].Char.AsciiChar= ' ';
@@ -157,9 +185,8 @@ void SetChar()
 void clearLine(int y)
 {
 	for(i=0;i<y;i++)
-		{
-		printf(" ");
-		}
+    printf(" ");
+}
 
 void GameOver()
 {
@@ -184,6 +211,7 @@ void GameOver()
 
 void Win()
 {
+	PlaySound("Win.wav", NULL, SND_FILENAME | SND_ASYNC);
 	system("cls");
 	gotoxy(35,6);printf("%c%c    %c%c     %c%c%c%c    %c%c    %c%c  %c%c%c  %c%c%c  %c%c%c\n",219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219);
 	gotoxy(35,7);printf(" %c%c  %c%c     %c%c  %c%c   %c%c    %c%c  %c%c%c  %c%c%c  %c%c%c\n",219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219);
@@ -198,7 +226,62 @@ void Win()
 	gotoxy(35,16);printf("%c%c   %c%c%c%c   %c%c  %c%c  %c%c  %c%c %c%c\n",219,219,219,219,219,219,219,219,219,219,219,219,219,219,219);
 	gotoxy(35,17);printf(" %c%c %c%c  %c%c %c%c   %c%c  %c%c   %c%c%c%c  %c%c%c  %c%c%c  %c%c%c\n",219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219);
 	gotoxy(35,18);printf("  %c%c%c    %c%c%c    %c%c  %c%c    %c%c%c  %c%c%c  %c%c%c  %c%c%c\n",219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219,219);
-	PlaySound("Win.wav", NULL, SND_FILENAME | SND_ASYNC);
+	
+	Sleep(5000);
+}
 
-	Sleep(500);
+void GetNama(char Nama[50])
+{
+    gotoxy(30,11);printf("============================================================");
+	gotoxy(30,12);printf("|                   Masukkan Nama Player                   |");
+	gotoxy(30,13);printf("============================================================");
+	gotoxy(30,14);printf("|                                                          |");
+    gotoxy(30,15);printf("============================================================");
+    gotoxy(32,14);fgets(Nama, 50, stdin);
+    Nama[strcspn(Nama, "\n")] = '\0';
+    system("cls");
+}
+
+void DisplayOption(int n,int M, char *options[M])
+{
+	gotoxy(30,13);printf("============================================================");
+	for(i=0;i<M+2;i++)
+	{
+		gotoxy(30,14+i);printf("|                                                          |");
+	}
+	gotoxy(30,16+M);printf("============================================================");
+    for (i = 0; i < M; i++) {
+        if (i == n) {
+            gotoxy(50,15+i);printf("\033[1;37;46m%-20s\033[0m ", options[i]); // Menyoroti opsi yang dipilih
+        } else {
+            gotoxy(50,15+i);printf("%s", options[i]);
+        }
+    }
+}
+
+int GetAksi()
+{
+	int Aksi;
+	while(1)
+	{
+		Aksi = getch() - 48;
+		if(Aksi > 3 || Aksi < 1)
+		{
+			if(Aksi== -21)
+			{
+				return 0;
+			}
+			else
+			{
+			gotoxy(35,28);printf("Invalid input!");
+			Beep(350,300);
+			gotoxy(35,28);clearLine(100);
+			continue;		
+			}
+		}else
+		{
+			break;
+		}	
+	}
+	return Aksi;
 }
